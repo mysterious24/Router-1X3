a667ab47835fe4dce6813cef7a2ab724c65a1c05
krishnakumar <102898555+mysterious24@users.noreply.github.com>
2024-02-11 23:34:06 +0530

    mux4_1.v

--- mux4_1.v
+++ mux4_1.v
@@ -1,26 +1,4 @@
-/********************************************************************************************
-Copyright 2019 - Maven Silicon Softech Pvt Ltd. 
- 
-All Rights Reserved.
 
-This source code is an unpublished work belongs to Maven Silicon Softech Pvt Ltd.
-
-It is not to be shared with or used by any third parties who have not enrolled for our paid training 
-
-courses or received any written authorization from Maven Silicon.
-
-
-Webpage     :      www.maven-silicon.com
-
-Filename    :	   mux4_1.v   
-
-Description :      Mux 4:1 design
-
-Author Name :      Susmita
-
-Version     :      1.0
-*********************************************************************************************/
- 
 module mux4_1(data_in,
 	      sel_in,
 	      y_out);
